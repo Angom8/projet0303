ab76ff66d89ef6fe7f48f75e4ace419a03c84f1e
angom8 <angom8@outlook.fr>
2019-11-14 11:59:13 +0100

    Contact Form + design

--- resources/views/contact.php
+++ resources/views/contact.php
@@ -7,24 +7,46 @@ getNavBar("Contact");
 <main>
 
 	<div class="row">
-  		<div class="col-sm-8">
+  		<div class="left-contact w-75">
 			<div class="container">
 				<h1>Contact</h1>
-				    <form method="post">
-					<label>Votre Nom</label>
-					<input type="text" name="nom" required><br>
-					<label>Votre Prenom</label>
-					<input type="text" name="prenom" required><br>
-					<label>Votre email</label>
-					<input type="email" name="email" placeholder="nom@exemple.com" onblur="checkmail()" required><br>
-					<label>Message</label>
-					<textarea name="message" required></textarea><br>
-					<label>Destinataire</label>
-					<input type="submit">
-				    </form>
+					<form>
+					    <div class="form-group ">
+					      <label for="inputNom">Nom</label>
+					      <input type="text" class="form-control" id="inputNom" placeholder="John">
+					    </div>
+					    <div class="form-group ">
+					      <label for="inputPrenom">Prenom</label>
+					      <input type="text" class="form-control" id="inputPrenom" placeholder="Doe">
+					    </div>
+					  <div class="form-group">
+					    <label for="inputMail">Mail</label>
+					    <input type="email" class="form-control" id="inputMail" placeholder="bateau@outlook.fr">
+					  </div>
+					
+					    <div class="form-group">
+					      <label for="inputProblem">Problem</label>
+					      <select id="inputProblem" class="form-control">
+						<option selected>Objet</option>
+						<option>J'ai rencontré un bogue</option>
+						<option>Je souhaite contacter l'administrateur du site</option>
+						<option>Je souhaite signaler une infraction</option>
+						<option>Je rencontre un autre problème</option>
+					      </select>
+					    </div>
+					  <div class="form-group">
+  <div class="form-group">
+    <label for="exampleFormControlTextarea1">Message</label>
+    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+  </div>
+					  </div>
+<button type="submit" class="btn btn-primary">Envoyer votre demande</button>
+					</form>
 			</div>
 		</div>
-  		<div class="col-sm-4 text-center right-contact">
+
+
+  		<div class="text-center right-contact w-25">
 			<div class="container">
 				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 			</div>
@@ -39,4 +61,4 @@ getFooter();
 
 getHTMLFooter();
 
-?>
+?>					  
