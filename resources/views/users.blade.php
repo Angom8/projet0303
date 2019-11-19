ff13f9855b51252407d99c2b4b4008d9328d9434
angom8 <angom8@outlook.fr>
2019-11-19 14:32:06 +0100

    L

--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -1,33 +1,36 @@
 <?php
 getHTMLHead("Liste des utilisateurs");
 
-getNavBar("");
+getAdmNavBar("Utilisateurs", "admin");
 ?>
 
 <main>
-<div class="container">
-  <h2>Utilisateurs</h2>
-  <table class="table">
-    <thead class="thead-dark">
-      <tr>
-        <th>NOM</th>
-        <th>PRENOM</th>
-        <th>DATE D'INSCRIPTION</th>
-		<th>NB BATEAUX</th>
-		<th>ID ADHERENT</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td>John</td>
-        <td>Doe</td>
-        <td>16/08/2019</td>
-		<td>1</td>
-		<td>0000000</td>
-      </tr>
-    </tbody>
-  </table>
- </div>
+	<div class ="content">
+		<div class="container">
+		  <h1>Utilisateurs</h1>
+		  <table class="table">
+		    <thead class="thead-dark">
+		      <tr>
+			<th>NOM</th>
+			<th>PRENOM</th>
+			<th>DATE D'INSCRIPTION</th>
+			<th>NB BATEAUX</th>
+			<th>ID ADHERENT</th>
+			<th>ACTION</th>
+		      </tr>
+		    </thead>
+		    <tbody>
+		      <tr>
+			<td>John</td>
+			<td>Doe</td>
+			<td>16/08/2019</td>
+				<td>1</td>
+				<td>0000000</td>
+		      </tr>
+		    </tbody>
+		  </table>
+		</div>
+	 </div>
  </main>
 <?php
 
