2c40265309de43b29ce6259ab74a0f7d169e6699
angom8 <angom8@outlook.fr>
2019-12-11 17:55:34 +0100

    Operational rank-based views

--- routes/web.php
+++ routes/web.php
@@ -28,55 +28,48 @@ Route::get('signin', function () {//done
     return view('signin');
 });
 
-
-/*do an auto redirection to match user-type and panel*/
-
-Route::get('mypanel', function () {//design improvment
-    return view('user-global');
-})->middleware('auth');
-
 /*Type Admin user pages*/
 
-Route::get('admin/', function () {//wip
+Route::get('admin/', function () {//done
     return view('user-global');
-})->middleware('auth');
+})->middleware('is_admin');
 
 Route::get('admin/messages', function () {
     return view('messages');
-})->middleware('auth');
+})->middleware('is_admin');
 
 Route::get('admin/add-boat', function () {
     return view('add-boat');
-})->middleware('auth');
+})->middleware('is_admin');
 
-Route::get('helper/add-fourni', function () {
-    return view('add-fourni');
-})->middleware('auth');
+Route::post('admin/add-fourni/register', ['uses' => 'FournisseurController@create', 'as' => 'fourni.register'])->middleware('is_admin');
 
-Route::post('admin/add-fourni/register', ['uses' => 'FournisseurController@create', 'as' => 'fourni.register'])->middleware('auth');
+Route::delete('admin/fournisseur/delete/{id}', ['uses' => 'FournisseurController@destroy', 'as' => 'fourni.delete'])->middleware('is_admin');//done
 
-Route::delete('admin/fournisseur/delete/{id}', ['uses' => 'FournisseurController@destroy', 'as' => 'fourni.delete'])->middleware('auth');//done
-
-/*Type Secretaire user pages*/
+/*Type Secretaire or Helper user pages*/
 Route::get('helper/', function () {//design improvment
     return view('user-global');
 })->middleware('auth');
 
-Route::get('helper/users',  ['uses' =>'ListesController@getUtilisateurs', 'as'=>'users'])->middleware('auth');//done
+Route::get('helper/users',  ['uses' =>'ListesController@getUtilisateurs', 'as'=>'users'])->middleware('is_helper');//done
 
-Route::get('helper/users/{id}',  ['uses' =>'ListesController@getUtilisateurs', 'as'=>'users'])->middleware('auth');//done
+Route::get('helper/users/{id}',  ['uses' =>'ListesController@getUtilisateurs', 'as'=>'users'])->middleware('is_helper');//done
 
-Route::delete('helper/user/del/{id}', ['uses' => 'UtilisateurController@destroy', 'as' => 'user.delete'])->middleware('auth');//done
+Route::delete('helper/user/del/{id}', ['uses' => 'UtilisateurController@destroy', 'as' => 'user.delete'])->middleware('is_helper');//done
 
-Route::get('helper/user/{id}', ['uses' => 'UtilisateurController@show', 'as' => 'user.show'])->middleware('auth');//done
+Route::get('helper/user/{id}', ['uses' => 'UtilisateurController@show', 'as' => 'user.show'])->middleware('is_helper');//done
 
-Route::post('helper/user/changePW', ['uses' => 'UtilisateurController@changePW', 'as' => 'user.changepw'])->middleware('auth');//done
+Route::post('helper/user/changePW', ['uses' => 'UtilisateurController@changePW', 'as' => 'user.changepw'])->middleware('is_helper');//done
 
-Route::get('helper/add-user', ['uses' => 'UtilisateurController@store', 'as' => 'user.store'])->middleware('auth');//done
+Route::get('helper/add-user', ['uses' => 'UtilisateurController@store', 'as' => 'user.store'])->middleware('is_helper');//done
 
-Route::post('helper/add-user/register', ['uses' => 'UtilisateurController@create', 'as' => 'user.register'])->middleware('auth');//done
+Route::post('helper/add-user/register', ['uses' => 'UtilisateurController@create', 'as' => 'user.register'])->middleware('is_helper');//done
 
 /*Type Adhérent user pages*/
+
+Route::get('mypanel', function () {//done
+    return view('user-global');
+})->middleware('auth');
 
 Route::get('panel/', function () {//done
     return view('user-global');
@@ -92,11 +85,7 @@ Route::get('panel/boats',  ['uses' =>'ListesController@getBateaux', 'as'=>'boats
 
 Route::get('panel/boats/{id}',  ['uses' =>'ListesController@getBateaux', 'as'=>'boatspagex'])->middleware('auth');//done
 
-Route::get('panel/boat/{id}',  ['uses' =>'BateauController@show', 'as'=>'boat.show'])->middleware('auth');//done
-
-Route::get('panel/boat', function () {
-    return view('boat');
-})->middleware('auth');
+Route::get('panel/boat/{id}',  ['uses' =>'BateauController@show', 'as'=>'boat.show'])->middleware('auth');//ajout piece if admin
 
 Route::get('panel/send-boat', function () {
     return view('send-boat');
